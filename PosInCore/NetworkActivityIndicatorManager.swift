7614a7d45004a58b1fc2c6eb65ab3b14468969ab
Alexandr Goncharov <ag@bekitzur.com>
2015-07-16 18:39:29 +0300

    add base network client

---
+++ PosInCore/NetworkActivityIndicatorManager.swift
@@ -0,0 +1,61 @@
+//
+//  NetworkActivityIndicatorManager.swift
+//  PositionIn
+//
+//  Created by Alexandr Goncharov on 16/07/15.
+//  Copyright (c) 2015 Soluna Labs. All rights reserved.
+//
+
+import Foundation
+
+@objc
+class NetworkActivityIndicatorManager {
+    private var activityCount: Int
+    private var activityIndicatorVisibilityTimer: NSTimer?
+    
+    var isNetworkActivityIndicatorVisible: Bool {
+        return activityCount > 0
+    }
+    
+    init() {
+        activityCount = 0
+    }
+    
+    func increment() {
+        objc_sync_enter(self)
+        activityCount++
+        objc_sync_exit(self)
+        
+        dispatch_async(dispatch_get_main_queue()) {
+            self.updateNetworkActivityIndicatorVisibilityDelayed()
+        }
+    }
+    
+    func decrement() {
+        objc_sync_enter(self)
+        activityCount = max(activityCount - 1, 0)
+        objc_sync_exit(self)
+        
+        dispatch_async(dispatch_get_main_queue()) {
+            self.updateNetworkActivityIndicatorVisibilityDelayed()
+        }
+    }
+    
+    private func updateNetworkActivityIndicatorVisibilityDelayed() {
+        // Delay hiding of activity indicator for a short interval, to avoid flickering
+        if (isNetworkActivityIndicatorVisible) {
+            dispatch_async(dispatch_get_main_queue()) {
+                self.updateNetworkActivityIndicatorVisibility()
+            }
+        } else {
+            activityIndicatorVisibilityTimer?.invalidate()
+            activityIndicatorVisibilityTimer = NSTimer(timeInterval: 0.2, target: self, selector: "updateNetworkActivityIndicatorVisibility", userInfo: nil, repeats: false)
+            activityIndicatorVisibilityTimer!.tolerance = 0.2
+            NSRunLoop.mainRunLoop().addTimer(activityIndicatorVisibilityTimer!, forMode: NSRunLoopCommonModes)
+        }
+    }
+    
+    func updateNetworkActivityIndicatorVisibility() {
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = isNetworkActivityIndicatorVisible
+    }
+}
---
+++ PosInCore/NetworkDataProvider.swift
@@ -0,0 +1,193 @@
+//
+//  NetworkDataProvider.swift
+//  PositionIn
+//
+//  Created by Alexandr Goncharov on 16/07/15.
+//  Copyright (c) 2015 Soluna Labs. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+public class NetworkDataProvider: NSObject {
+    /**
+    Execute request and parse response
+    
+    :param: URLRequest The URL request
+    :param: map        Response mapping function
+    :param: completion Completion block
+    
+    :returns: The created request
+    */
+    public  func jsonRequest<U,V>(
+        URLRequest: Alamofire.URLRequestConvertible,
+        map: AnyObject?->U?,
+        completion: (OperationResult<V>)->Void
+        ) -> Alamofire.Request {
+            
+            let serializer = Alamofire.Request.CustomResponseSerializer(map)
+            activityIndicator.increment()
+            let r = request(URLRequest).response(serializer: serializer, completionHandler: {
+                [unowned self] (request, response, object, error) in
+                self.activityIndicator.decrement()
+                if let object = object as? Box<V> {
+                    completion(.Success(object))
+                } else {
+                    completion(failure(error ?? ErrorCodes.InvalidResponseError.error()))
+                }
+                })
+            
+            return r
+    }
+    
+    /// Used API
+    public let apiService: APIService
+    
+    
+    /**
+    Designated initializer
+    
+    :param: api           api service
+    :param: configuration session configuration
+    
+    :returns: new instance
+    */
+    public init(
+        api: APIService,
+        configuration: NSURLSessionConfiguration = NSURLSessionConfiguration.defaultSessionConfiguration()
+        ) {
+            apiService = api
+            manager = Alamofire.Manager(configuration: configuration)
+    }
+    
+    /// Singleton instance
+//    public class var sharedInstance: NetworkDataProvider {
+//        struct Static {
+//            static var onceToken: dispatch_once_t = 0
+//            static var instance: NetworkDataProvider? = nil
+//        }
+//        dispatch_once(&Static.onceToken) {
+//            Static.instance = NetworkDataProvider(api: .Dev)
+//        }
+//        return Static.instance!
+//    }
+
+    private let manager: Alamofire.Manager
+    private let activityIndicator = NetworkActivityIndicatorManager()
+    
+    internal func request(URLRequest: Alamofire.URLRequestConvertible) -> Alamofire.Request {
+        let request = manager.request(URLRequest).validate()
+        
+        #if DEBUG
+            println(request.debugDescription)
+        #endif
+        
+        return request
+    }
+}
+
+/**
+ Protocol that wrap url generation
+*/
+public protocol APIService: Printable {
+    /**
+    Returns http request string
+    
+    :param: endpoint       endpoint
+    
+    :returns: url string
+    */
+    func http(endpoint: String) -> String
+    /**
+    Returns http request string
+    
+    :param: endpoint       endpoint
+    
+    :returns: url string
+    */
+    func https(endpoint: String) -> String
+}
+
+
+private extension Alamofire.Request {
+    
+    //MARK: - Custom serializer -
+    class func CustomResponseSerializer<T>(mapping:AnyObject? -> T?) -> Serializer {
+        
+        return { (request, response, data) in
+            let JSONSerializer = Request.JSONResponseSerializer(options: .AllowFragments)
+            let (json: AnyObject?, serializationError) = JSONSerializer(request, response, data)
+            switch (response, json, serializationError) {
+            case (.None, _, _):
+                return (nil, NetworkDataProvider.ErrorCodes.TransferError.error())
+            case (_, _, .Some(let error)):
+                return (nil, NetworkDataProvider.ErrorCodes.ParsingError.error(underlyingError: error))
+            default:
+                if let object  = mapping(json) {
+                    return (Box(object), nil)
+                } else {
+                    return (nil, NetworkDataProvider.ErrorCodes.InvalidResponseError.error())
+                }
+            } // switch
+        }
+    }
+    
+}
+
+//MARK: - Network errors -
+
+extension NetworkDataProvider {
+    /**
+    Network error codes
+    
+    - UnknownError:    Unknown error
+    - InvalidRequestError:  Invalid request error
+    - TransferError:   Transfer error
+    - InvalidResponseError: Invalid response error
+    - ParsingError:    Response Parsing error
+    */
+    enum ErrorCodes: Int {
+        static let errorDomain = "com.bekitzur.Network"
+        
+        case UnknownError, InvalidRequestError, TransferError, InvalidResponseError, ParsingError
+        
+        /**
+        Converting Error code to the NSError
+        
+        :param: underlyingError underlying error
+        
+        :returns: NSError instance
+        */
+        func error(underlyingError: NSError? = nil) -> NSError {
+            let description = NSString(format: NSLocalizedString("Network error: %@", comment: "Localized network error description"),
+                self.reason)
+            var userInfo: [NSObject : AnyObject] = [
+                NSLocalizedDescriptionKey: description,
+                NSLocalizedFailureReasonErrorKey: self.reason,
+            ]
+            
+            if let underlyingError = underlyingError {
+                userInfo[NSUnderlyingErrorKey] = underlyingError
+            }
+            return NSError(domain:ErrorCodes.errorDomain, code: self.rawValue, userInfo: userInfo)
+        }
+        
+        /// Localized failure reason
+        var reason: String {
+            switch self {
+            case .InvalidRequestError:
+                return NSLocalizedString("InvalidRequestError", comment: "Invalid request")
+            case .InvalidResponseError:
+                return NSLocalizedString("InvalidResponseError", comment: "Invalid response")
+            case .ParsingError:
+                return NSLocalizedString("ParsingError", comment: "Parsing error")
+            case .TransferError:
+                return NSLocalizedString("TransferError", comment: "Data transfer error")
+            case .UnknownError:
+                fallthrough
+            default:
+                return NSLocalizedString("UnknownError", comment: "Unknown error")
+            }
+        }
+    }
+}
