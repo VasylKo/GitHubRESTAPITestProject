63b775bf7d919f3f8c99d9e5b1456320a90e401f
Alexandr Goncharov <ag@bekitzur.com>
2015-08-03 16:50:05 +0300

    move transition in separate file

--- PositionIn/Search/SearchViewController.swift
+++ PositionIn/Search/SearchViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import UIImageEffects
+
 
 class SearchViewController: UIViewController {
 
@@ -22,6 +22,11 @@ class SearchViewController: UIViewController {
         tableView.hidden = true
     }
     
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        categoriesSearchBar.becomeFirstResponder()
+    }
+    
     class func present(searchBar: SearchBar, presenter: UIViewController) {
         let searchController = Storyboards.Main.instantiateSearchViewController()
         let transitionDelegate = SearchTransitioningDelegate()
@@ -29,78 +34,5 @@ class SearchViewController: UIViewController {
         searchController.transitioningDelegate = transitionDelegate
         presenter.presentViewController(searchController, animated: true) {            
         }
-    }
-}
-
-
-class SearchTransitioningDelegate: NSObject, UIViewControllerTransitioningDelegate {
-    weak var startView: UIView?
-    
-    func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        if let fromView = startView {
-            let startFrame = fromView.convertRect(fromView.frame, toView: presenting.view)
-            let animationController = SearchAppearanceAnimationController(startFrame: startFrame)
-            return animationController
-        }
-        return nil
-    }
-}
-
-class SearchAppearanceAnimationController: NSObject,UIViewControllerAnimatedTransitioning {
-    let transitionDuration: NSTimeInterval = 0.5
-    let startFrame: CGRect
-    
-    override convenience init() {
-        self.init(startFrame: CGRectZero)
-    }
-    
-    init(startFrame: CGRect) {
-        self.startFrame = startFrame
-        super.init()
-    }
-    
-    // This is used for percent driven interactive transitions, as well as for container controllers that have companion animations that might need to
-    // synchronize with the main animation.
-    func transitionDuration(transitionContext: UIViewControllerContextTransitioning) -> NSTimeInterval {
-        return transitionDuration
-    }
-    
-    // This method can only  be a nop if the transition is interactive and not a percentDriven interactive transition.
-    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
-        let conrainerView = transitionContext.containerView()
-        let searchController = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey) as! SearchViewController
-        let fromViewController = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)!
-        searchController.backImageView.image = snapshotView(fromViewController.view)
-        fromViewController.view.removeFromSuperview()
-        let searchView = searchController.view
-        conrainerView.addSubview(searchView)
-        
-        let categoriesFrame = searchController.categoriesSearchBar.frame
-        searchController.categoriesSearchBar.frame = startFrame
-        let locationsFrame = searchController.locationSearchBar.frame
-        searchController.locationSearchBar.frame = startFrame
-        UIView.animateWithDuration(
-            transitionDuration(transitionContext),
-            delay: 0,
-            options: UIViewAnimationOptions.CurveEaseInOut | UIViewAnimationOptions.BeginFromCurrentState,
-            animations: { 
-                searchController.categoriesSearchBar.frame = categoriesFrame
-                searchController.locationSearchBar.frame = locationsFrame
-        }) { finished in
-            transitionContext.completeTransition(finished)
-        }
-    }
-    
-//    // This is a convenience and if implemented will be invoked by the system when the transition context's completeTransition: method is invoked.
-//    func animationEnded(transitionCompleted: Bool) {
-//        
-//    }
-    
-    private func snapshotView(view: UIView) -> UIImage {
-        UIGraphicsBeginImageContext(view.bounds.size)
-        view.drawViewHierarchyInRect(view.bounds, afterScreenUpdates: false)
-        let image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return image.applyLightEffect()
     }
 }
