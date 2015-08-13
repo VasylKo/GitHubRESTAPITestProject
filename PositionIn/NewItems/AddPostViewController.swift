0059de2ca74d1e02b1fd97625d49f27f497038f7
Alexandr Goncharov <ag@bekitzur.com>
2015-08-13 12:07:45 +0300

    update new post form

--- PositionIn/NewItems/AddPostViewController.swift
+++ PositionIn/NewItems/AddPostViewController.swift
@@ -18,14 +18,9 @@ import Photos
 
 class AddPostViewController: XLFormViewController {
     private enum Tags : String {
-        case Title = "Title"
-        case Price = "Price"
-        case Category = "Category"
-        case StartDate = "Start date"
-        case EndDate = "End date"
+        case Message = "Message"
         case Community = "Community"
         case Photo = "Photo"
-        case Location = "Location"
     }
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
@@ -39,47 +34,23 @@ class AddPostViewController: XLFormViewController {
     }
 
     func initializeForm() {
-        let form = XLFormDescriptor(title: NSLocalizedString("New Product", comment: "New product: form caption"))
+        let form = XLFormDescriptor(title: NSLocalizedString("New Post", comment: "New post: form caption"))
         
         // Description section
         let descriptionSection = XLFormSectionDescriptor.formSection()
         form.addFormSection(descriptionSection)
-        // Title
-        let titleRow = XLFormRowDescriptor(tag: Tags.Title.rawValue, rowType: XLFormRowDescriptorTypeText)
-        titleRow.cellConfigAtConfigure["textField.placeholder"] = NSLocalizedString("Title", comment: "New product: title")
-        titleRow.required = true
-        descriptionSection.addFormRow(titleRow)
-        // Price
-        let priceRow = XLFormRowDescriptor(tag: Tags.Price.rawValue, rowType: XLFormRowDescriptorTypeDecimal, title: NSLocalizedString("Price ($)", comment: "New product: price"))
-        descriptionSection.addFormRow(priceRow)
+        // Message
+        let messageRow = XLFormRowDescriptor(tag: Tags.Message.rawValue, rowType:XLFormRowDescriptorTypeTextView)
+        messageRow.cellConfigAtConfigure["textView.placeholder"] = NSLocalizedString("Message", comment: "New post: message")
+        descriptionSection.addFormRow(messageRow)
+
         
         // Info section
         let infoSection = XLFormSectionDescriptor.formSection()
         form.addFormSection(infoSection)
-        // Category
-        let categoryCaption = NSLocalizedString("Category", comment: "New product: category caption")
-        let categoryRow = XLFormRowDescriptor(tag: Tags.Category.rawValue, rowType:XLFormRowDescriptorTypeMultipleSelector, title: categoryCaption)
-        categoryRow.value = [ XLFormOptionsObject(value: 0, displayText: "Other") ]
-        categoryRow.selectorTitle = categoryCaption
-        categoryRow.selectorOptions = [
-            XLFormOptionsObject(value: 0, displayText: "Other"),
-            XLFormOptionsObject(value: 1, displayText: "Category 1"),
-            XLFormOptionsObject(value: 2, displayText: "Category 2"),
-            XLFormOptionsObject(value: 3, displayText: "Category 3"),
-            XLFormOptionsObject(value: 4, displayText: "Category 4"),
-            XLFormOptionsObject(value: 5, displayText: "Category 5")
-        ]
-        infoSection.addFormRow(categoryRow)
-        // Location
-        let locationRow = XLFormRowDescriptor(tag: Tags.Location.rawValue, rowType: XLFormRowDescriptorTypeSelectorPush, title: NSLocalizedString("Location", comment: "New product: location"))
-        locationRow.action.formSegueClass = NSClassFromString("UIStoryboardPushSegue")
-        locationRow.action.viewControllerClass = LocationSelectorViewController.self
-        locationRow.valueTransformer = CLLocationValueTrasformer.self
-        locationRow.value = CLLocation(latitude: -33, longitude: -56)
-        infoSection.addFormRow(locationRow)
 
         // Community
-        let communityCaption = NSLocalizedString("Community", comment: "New product: comunity caption")
+        let communityCaption = NSLocalizedString("Community", comment: "New post: comunity caption")
         let communityRow = XLFormRowDescriptor(tag: Tags.Community.rawValue, rowType:XLFormRowDescriptorTypeSelectorPush, title: communityCaption)
         communityRow.selectorTitle = communityCaption
         communityRow.value =  XLFormOptionsObject(value: 0, displayText:"All")
@@ -94,24 +65,10 @@ class AddPostViewController: XLFormViewController {
         let photoSection = XLFormSectionDescriptor.formSection()
         form.addFormSection(photoSection)
         //Photo row
-        let photoRow = XLFormRowDescriptor(tag: Tags.Photo.rawValue, rowType: XLFormRowDescriptorTypeButton, title: NSLocalizedString("Insert photo", comment: "New product: insert photo"))
+        let photoRow = XLFormRowDescriptor(tag: Tags.Photo.rawValue, rowType: XLFormRowDescriptorTypeButton, title: NSLocalizedString("Insert photo", comment: "New post: insert photo"))
         photoRow.cellConfig["textLabel.textColor"] = UIColor(red: 0.0, green: 122.0/255.0, blue: 1.0, alpha: 1.0)
         photoRow.action.formSelector = "didTouchPhoto:"
-        photoSection.addFormRow(photoRow)
-
-        //Dates section
-        let datesSection = XLFormSectionDescriptor.formSectionWithTitle(NSLocalizedString("Pick-up Availability (Optional)", comment: "New product: dates section header"))
-        form.addFormSection(datesSection)
-        //Start date
-        let startDate = XLFormRowDescriptor(tag: Tags.StartDate.rawValue, rowType: XLFormRowDescriptorTypeDateTimeInline, title: NSLocalizedString("Start date", comment: "New product: Start date"))
-        startDate.value = NSDate(timeIntervalSinceNow: 60*60*24)
-        datesSection.addFormRow(startDate)
-        //End date
-        let endDate = XLFormRowDescriptor(tag: Tags.EndDate.rawValue, rowType: XLFormRowDescriptorTypeDateTimeInline, title: NSLocalizedString("End date", comment: "New product: End date"))
-        endDate.value = NSDate(timeIntervalSinceNow: 60*60*25)
-        datesSection.addFormRow(endDate)
-
-        
+        photoSection.addFormRow(photoRow)        
         
         self.form = form
     }
