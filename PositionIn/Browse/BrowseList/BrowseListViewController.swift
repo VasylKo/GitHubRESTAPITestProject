fe5ece8dac04319352e2e9fc1151357ab97b4243
Alex <ag@bekitzur.com>
2015-08-20 19:47:45 +0300

    added filtering

--- PositionIn/Browse/BrowseList/BrowseListViewController.swift
+++ PositionIn/Browse/BrowseList/BrowseListViewController.swift
@@ -18,7 +18,19 @@ final class BrowseListViewController: UIViewController, BrowseActionProducer {
     
     var filter: Filter = .All {
         didSet {
-            tableView?.reloadData()
+            switch filter {
+            case .All:
+                dataSource = GeneralListDataSource()
+            case .Posts:
+                dataSource = PostListDataSource()
+            case .Products:
+                dataSource = ProductListDataSource()
+            case .Promotions:
+                dataSource = PromotionListDataSource()
+            case .Events:
+                dataSource = EventListDataSource()
+            }
+            dataSource.configureTable(tableView)
         }
     }
     
@@ -37,8 +49,8 @@ final class BrowseListViewController: UIViewController, BrowseActionProducer {
         }
     }
     
-    private lazy var dataSource: ProductListDataSource = {
-        let dataSource = ProductListDataSource()
+    private lazy var dataSource: TableViewDataSource = {
+        let dataSource = GeneralListDataSource()
         dataSource.parentViewController = self
         return dataSource
         }()
@@ -52,7 +64,7 @@ final class BrowseListViewController: UIViewController, BrowseActionProducer {
 
 
 extension BrowseListViewController {
-    internal class ProductListDataSource: TableViewDataSource {
+    internal class GeneralListDataSource: TableViewDataSource {
                 
         override func configureTable(tableView: UITableView) {
             tableView.estimatedRowHeight = 80.0
@@ -107,4 +119,134 @@ extension BrowseListViewController {
             }
         }
     }
+    
+    internal class EventListDataSource: TableViewDataSource {
+        
+        override func configureTable(tableView: UITableView) {
+            tableView.estimatedRowHeight = 80.0
+            super.configureTable(tableView)
+        }
+        
+        @objc override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 100
+        }
+        
+        @objc override func tableView(tableView: UITableView, reuseIdentifierForIndexPath indexPath: NSIndexPath) -> String {
+            return  EventListCell.reuseId()
+        }
+        
+        override func tableView(tableView: UITableView, modelForIndexPath indexPath: NSIndexPath) -> TableViewCellModel {
+            return TableViewCellEventModel(title: "Art Gallery", date: NSDate(), info: "45 People are attending", imageURL: "https://www.daycounts.com/images/stories/virtuemart/product/Virtuemart_Bundl_4f6eaee37356e.png")
+        }
+        
+        override func nibCellsId() -> [String] {
+            return [EventListCell.reuseId()]
+        }
+        
+        func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+            tableView.deselectRowAtIndexPath(indexPath, animated: true)
+            if let browseController = parentViewController as? BrowseActionProducer,
+                let actionConsumer = browseController.actionConsumer {
+                    actionConsumer.browseController(browseController, didSelectPost: Post(objectId: CRUDObjectInvalidId))
+            }
+        }
+    }
+    
+    internal class ProductListDataSource: TableViewDataSource {
+        
+        override func configureTable(tableView: UITableView) {
+            tableView.estimatedRowHeight = 80.0
+            super.configureTable(tableView)
+        }
+        
+        @objc override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 100
+        }
+        
+        @objc override func tableView(tableView: UITableView, reuseIdentifierForIndexPath indexPath: NSIndexPath) -> String {
+            return ListProductCell.reuseId()
+        }
+        
+        override func tableView(tableView: UITableView, modelForIndexPath indexPath: NSIndexPath) -> TableViewCellModel {
+            return TableViewCellProductModel(title: "The forest", owner: "Edwarn Ryan", distance: 0.09, imageURL: "https://www.daycounts.com/images/stories/virtuemart/product/Virtuemart_Bundl_4f6eaee37356e.png", price: 99.8)
+        }
+        
+        override func nibCellsId() -> [String] {
+            return [ListProductCell.reuseId()]
+        }
+        
+        func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+            tableView.deselectRowAtIndexPath(indexPath, animated: true)
+            if let browseController = parentViewController as? BrowseActionProducer,
+                let actionConsumer = browseController.actionConsumer {
+                    actionConsumer.browseController(browseController, didSelectPost: Post(objectId: CRUDObjectInvalidId))
+            }
+        }
+    }
+
+    internal class PromotionListDataSource: TableViewDataSource {
+        
+        override func configureTable(tableView: UITableView) {
+            tableView.estimatedRowHeight = 80.0
+            super.configureTable(tableView)
+        }
+        
+        @objc override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 100
+        }
+        
+        @objc override func tableView(tableView: UITableView, reuseIdentifierForIndexPath indexPath: NSIndexPath) -> String {
+            return PromotionListCell.reuseId()
+        }
+        
+        override func tableView(tableView: UITableView, modelForIndexPath indexPath: NSIndexPath) -> TableViewCellModel {
+            return TableViewCellPromotionModel(title: "Arts & Crafts Summer Sale", author: "The Sydney Art Store", discount: "Save 80%", imageURL: "https://www.daycounts.com/images/stories/virtuemart/product/Virtuemart_Bundl_4f6eaee37356e.png")
+        }
+        
+        override func nibCellsId() -> [String] {
+            return [PromotionListCell.reuseId()]
+        }
+        
+        func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+            tableView.deselectRowAtIndexPath(indexPath, animated: true)
+            if let browseController = parentViewController as? BrowseActionProducer,
+                let actionConsumer = browseController.actionConsumer {
+                    actionConsumer.browseController(browseController, didSelectPost: Post(objectId: CRUDObjectInvalidId))
+            }
+        }
+    }
+
+    internal class PostListDataSource: TableViewDataSource {
+        
+        override func configureTable(tableView: UITableView) {
+            tableView.estimatedRowHeight = 80.0
+            super.configureTable(tableView)
+        }
+        
+        @objc override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 100
+        }
+        
+        @objc override func tableView(tableView: UITableView, reuseIdentifierForIndexPath indexPath: NSIndexPath) -> String {
+            return PostListCell.reuseId()
+        }
+        
+        override func tableView(tableView: UITableView, modelForIndexPath indexPath: NSIndexPath) -> TableViewCellModel {
+            return TableViewCellPostModel(title: "Betty Wheeler", info: "Lovely day to go golfing", imageURL: "https://www.daycounts.com/images/stories/virtuemart/product/Virtuemart_Bundl_4f6eaee37356e.png")
+        }
+        
+        override func nibCellsId() -> [String] {
+            return [PromotionListCell.reuseId()]
+        }
+        
+        func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+            tableView.deselectRowAtIndexPath(indexPath, animated: true)
+            if let browseController = parentViewController as? BrowseActionProducer,
+                let actionConsumer = browseController.actionConsumer {
+                    actionConsumer.browseController(browseController, didSelectPost: Post(objectId: CRUDObjectInvalidId))
+            }
+        }
+    }
+
+
 }
