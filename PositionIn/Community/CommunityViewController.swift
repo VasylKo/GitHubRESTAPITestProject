f5ffe291676090f2f36d5c679c570076e50dcee0
Alexandr Goncharov <ag@bekitzur.com>
2015-09-06 19:51:54 +0300

    open community feed

--- PositionIn/Community/CommunityViewController.swift
+++ PositionIn/Community/CommunityViewController.swift
@@ -15,6 +15,9 @@ final class CommunityViewController: BrowseModeViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.hidesBackButton = false
+        self.navigationItem.leftBarButtonItem = nil
+
         displayMode = .List
     }
     
