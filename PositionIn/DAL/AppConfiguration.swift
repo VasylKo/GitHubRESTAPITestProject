2a8a66ee672c16614d027a3c87353993e17f2912
Alexandr Goncharov <ag@bekitzur.com>
2015-10-07 07:26:09 +0300

    add staging and prod environments

--- PositionIn/DAL/AppConfiguration.swift
+++ PositionIn/DAL/AppConfiguration.swift
@@ -10,10 +10,23 @@ import Foundation
 
 final class AppConfiguration {
     init() {
+        #if PROD_ENV
+        //Prod
+        baseURL = NSURL(string: "https://positionin.com/api/")!
+        amazonURL = NSURL(string: "https://pos-prod.s3.amazonaws.com/")!
+        xmppHostname = "positionin.com"
+        #elseif STAGING_ENV
+        //Staging
+        baseURL = NSURL(string: "https://app-sta.positionin.com/api/")!
+        amazonURL = NSURL(string: "https://pos-sta.s3.amazonaws.com/")!
+        xmppHostname = "app-sta.positionin.com"
+        #else
+        //Dev
         baseURL = NSURL(string: "https://app-dev.positionin.com/api/")!
         amazonURL = NSURL(string: "https://pos-dev.s3.amazonaws.com/")!
-        googleMapsKey = "AIzaSyA3NvrDKBcpIsnq4-ZACG41y7Mj-wSfVrY"
         xmppHostname = "app-dev.positionin.com"
+        #endif
+        googleMapsKey = "AIzaSyA3NvrDKBcpIsnq4-ZACG41y7Mj-wSfVrY"
         xmppPort = 5222
     }
     
