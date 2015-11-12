1df48af7ef07b8a0a59c2cdaf9c4ef818e0a930c
Alexandr Goncharov <ag@bekitzur.com>
2015-11-12 20:53:48 +0300

    update snapshot config

--- PositionInUITests/PositionInUITests.swift
+++ PositionInUITests/PositionInUITests.swift
@@ -12,13 +12,16 @@ class PositionInUITests: XCTestCase {
         
     override func setUp() {
         super.setUp()
+        let app = XCUIApplication()
+        setLanguage(app)
+        
         
         // Put setup code here. This method is called before the invocation of each test method in the class.
         
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -28,9 +31,16 @@ class PositionInUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testLogin() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        snapshot("01.LoginScreen")
     }
+    
+//    func testHome() {
+//        XCUIApplication().navigationBars["PositionIn.LoginSignupView"].buttons["Skip"].tap()
+//        snapshot("02.HomeScreen")
+//    }
     
 }
