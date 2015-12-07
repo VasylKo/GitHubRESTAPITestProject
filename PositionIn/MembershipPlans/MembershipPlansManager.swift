ea46a9a626750710f1ca3f6e4bb3584eaf33ac48
Mikhail Polyevin <mpol@bekitzur.com>
2015-12-07 20:22:33 +0300

    update texts in membership plans

--- PositionIn/MembershipPlans/MembershipPlansManager.swift
+++ PositionIn/MembershipPlans/MembershipPlansManager.swift
@@ -20,21 +20,21 @@ enum IndividualPlans: Int {
         case SchoolMember:
             return ["Participate in KRCS youth club activities in school/college",
                 "Participate in localizing and implementation of the four part youth program",
-                "Participate in Formulation on and strategies for the youth club"]
+                "Participate in the formulation of strategies for the youth club"]
         case YouthMember:
-            return ["Vote, to elect and be elected as members of the different committees and Boards within the Kenya Red Cross Society governance structure.",
-                "Linking the society with resourceful strategic partnerships" ]
+            return ["Vote to elect and to be elected as members of the different committees and boards within the Kenyan Red Cross Society governance structure.",
+                "Linking society with resourceful strategic partnerships"]
         case OrdinaryMember:
-            return ["Vote, to elect and be elected as members of the different committees and Boards within the Kenya Red Cross Society governance structure",
-                "Invitations to attend special events e.g Life Members Day, KRC Gala, World Red Cross Day",
+            return ["Vote to elect and to be elected as members of the different committees and boards within the Kenyan Red Cross Society governance structure",
+                "Invitations to attend special events, e.g., Life Members Day, KRC Gala, World Red Cross Day",
                 "Participate in decision making, policy formulation and constitutional making/review",
                 "Linking the society with resourceful strategic partnerships"]
         case LifeMember:
             return ["Discount of KRCS related merchandise to partners",
-                "Upto 5% of discount for one year membership at Eplus",
-                "Upto 15% disount at the Boma",
-                "Invitations to attend special events e.g Life Members Day, KRCS Gala, World Red Cross Day",
-                "Up to date information on KRCS operations through; emails i.e. KRCS E-news",
+                "Up to 5% of discount for one year membership at EPlus",
+                "Up to 15% discount at the Boma",
+                "Invitations to attend special events, e.g., Life Members Day, KRC Gala, World Red Cross Day",
+                "Up-to-date information on KRCS operations through e-mails such as KRC E-news",
                 "Update on emergency precautions"
             ]
         }
@@ -60,7 +60,7 @@ enum IndividualPlans: Int {
         case Guest:
             return NSLocalizedString("Continue as Guest", comment: "Membership")
         case SchoolMember:
-            return NSLocalizedString("Youth Member is School", comment: "Membership")
+            return NSLocalizedString("Youth Member in School", comment: "Membership")
         case YouthMember:
             return NSLocalizedString("Youth Member Over 18", comment: "Membership")
         case OrdinaryMember:
@@ -117,8 +117,16 @@ enum CorporatePlans: Int {
         case Silverline:
             fallthrough
         case GoldPremiere:
-            return ["Upto 5% of discount for one year membership at Eplus", "Upto 15% disount at the Boma", "Invitations to attend special events e.g Life Members Day, KRC Gala, World Red Cross Day", "Text sent to them to wish them a happy from KRCS or any other thing that they will be celebrating.", "One Member per quarter to be featured in the reach out.",
-                "Up to date information on KRCS operations through; emails i.e. KRC E-news", "Invitation to programe launches", "Update on emergency precautions", "School clubs will receive free basic FirstAid training"]
+            return [
+                "Up to 5% of discount for one year membership at EPlus",
+                "Up to 15% discount at the Boma",
+                "Invitations to attend special events, e.g., Life Members Day, KRC Gala, World Red Cross Day",
+                "Message sent to them from KRCS with birthday wishes or with greetings towards what they celebrate.",
+                "One member per quarter to be featured in the outreach.",
+                "Up-to-date information on KRCS operations through e-mails such as KRC E-news",
+                "Invitation to program launches",
+                "Update on emergency precautions",
+                "School clubs will receive free basic first aid training"]
         }
     }
     
