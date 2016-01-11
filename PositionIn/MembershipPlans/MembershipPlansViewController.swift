e704f46a5c9ea78ac2bdd1dc3dcbd599fbe67d0b
Nikita Galimzyanov <ng@bekitzur.com>
2016-01-11 16:03:33 +0300

    Back button (navigation) added to Membership screen

--- PositionIn/MembershipPlans/MembershipPlansViewController.swift
+++ PositionIn/MembershipPlans/MembershipPlansViewController.swift
@@ -9,11 +9,8 @@
 class MembershipPlansViewController: UIViewController {
 
     override func viewDidLoad() {
-        self.title = NSLocalizedString("Membership Plans", comment: "Membership")
         super.viewDidLoad()
-        
-        let backButton = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.Plain, target: navigationController, action: nil)
-        navigationItem.leftBarButtonItem = backButton
+        self.title = NSLocalizedString("Membership Plans", comment: "Membership")
     }
     
     override func viewWillAppear(animated: Bool) {
