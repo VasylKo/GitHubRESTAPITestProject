8f14362bd6bcf5e14f98dd844a8ff4a1ef9831a7
Nikita Galimzyanov <ng@bekitzur.com>
2016-01-19 17:56:22 +0300

    refactor

--- PosInCore/NetworkActivityIndicatorManager.swift
+++ PosInCore/NetworkActivityIndicatorManager.swift
@@ -8,18 +8,12 @@
 
 import Foundation
 
-
-class NetworkActivityIndicatorManager: NSObject {
-    private var activityCount: Int
+class NetworkActivityIndicatorManager {
+    private var activityCount: Int = 0
     private var activityIndicatorVisibilityTimer: NSTimer?
     
     var isNetworkActivityIndicatorVisible: Bool {
         return activityCount > 0
-    }
-    
-    override init() {
-        activityCount = 0
-        super.init()
     }
     
     func increment() {
