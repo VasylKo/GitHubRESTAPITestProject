b062baa6e17687194ffc2daf3b3682eceb4c8e7e
Nikita Galimzyanov <ng@bekitzur.com>
2016-02-17 20:27:36 +0300

    likes added

---
+++ PositionIn/CellModels/NewsCellModelFactory.swift
@@ -0,0 +1,28 @@
+//
+//  NewsCellModelFactory.swift
+//  PositionIn
+//
+//  Created by ng on 2/17/16.
+//  Copyright © 2016 Soluna Labs. All rights reserved.
+//
+
+import Foundation
+import PosInCore
+import CleanroomLogger
+
+struct NewsCellModelFactory {
+    
+    func model(item: FeedItem, actionConsumer: NewsListActionConsumer?) -> [[TableViewCellModel]] {
+        let model = NewsTableViewCellModel(item: item, actionConsumer: actionConsumer)
+        return [[model]]
+    }
+    
+    func cellsReuseId() -> [String] {
+        return [NewsCardCell.reuseId()]
+    }
+    
+    func cellReuseIdForModel(model: TableViewCellModel) -> String {
+        return NewsCardCell.reuseId()
+    }
+    
+}
