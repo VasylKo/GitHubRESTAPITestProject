ef9a91458ef1f147cdd7d5c68e4b20141e53bc6a
Ruslan Kolchakov <rkolchakov@bekitzur.com>
2016-03-09 15:46:12 +0300

    POS-1730, POS-1731: Fixed info on wallet cell

--- PositionIn/Views/FeedListCells/EventListCell.swift
+++ PositionIn/Views/FeedListCells/EventListCell.swift
@@ -10,12 +10,12 @@ import PosInCore
 import UIKit
 
 final class EventListCell: TableViewCell {
+    @IBOutlet weak var productImage: UIImageView?
+    @IBOutlet weak var titleLabel: UILabel?
+    @IBOutlet weak var dateLabel: UILabel?
+    @IBOutlet weak var infoLabel: UILabel?
     
-    @IBOutlet private weak var productImage: UIImageView!
-    @IBOutlet private weak var titleLabel: UILabel!
-    @IBOutlet private weak var dateLabel: UILabel!
-    @IBOutlet private weak var infoLabel: UILabel!
-    
+    // MARK: - TableViewCell
     override func setModel(model: TableViewCellModel) {
         let m = model as? CompactFeedTableCellModel
         assert(m != nil, "Invalid model passed")
@@ -24,38 +24,50 @@ final class EventListCell: TableViewCell {
         
         switch m!.item.type {
         case .Emergency:
-            productImage.setImageFromURL(m!.imageURL, placeholder: UIImage(named: "PromotionDetailsPlaceholder"))
+            productImage?.setImageFromURL(m!.imageURL, placeholder: UIImage(named: "PromotionDetailsPlaceholder"))
+            configureAttendingInfo(m!.numOfParticipants ?? 0)
         case .GiveBlood:
-            productImage.setImageFromURL(m!.imageURL, placeholder: UIImage(named: "give_blood_img_default"))
+            productImage?.setImageFromURL(m!.imageURL, placeholder: UIImage(named: "give_blood_img_default"))
+            configureAttendingInfo(m!.numOfParticipants ?? 0)
         case .Training:
-            productImage.setImageFromURL(m!.imageURL, placeholder: UIImage(named: "trainings_placeholder"))
+            productImage?.setImageFromURL(m!.imageURL, placeholder: UIImage(named: "trainings_placeholder"))
+            configureAttendingInfo(m!.numOfParticipants ?? 0)
         case .Market:
-            productImage.setImageFromURL(m!.imageURL, placeholder: UIImage(named: "market_img_default"))
+            productImage?.setImageFromURL(m!.imageURL, placeholder: UIImage(named: "market_img_default"))
+            configureAttendingInfo(m!.numOfParticipants ?? 0)
         case .BomaHotels:
-            productImage.setImageFromURL(m!.imageURL, placeholder: UIImage(named: "bomaHotelPlaceholder"))
+            productImage?.setImageFromURL(m!.imageURL, placeholder: UIImage(named: "bomaHotelPlaceholder"))
+            configureAttendingInfo(m!.numOfParticipants ?? 0)
         case .Project:
-            productImage.setImageFromURL(m!.imageURL, placeholder: UIImage(named: "hardware_img_default"))
+            productImage?.setImageFromURL(m!.imageURL, placeholder: UIImage(named: "hardware_img_default"))
+            configureAttendingInfo(m!.numOfParticipants ?? 0)
         case .Event:
-            productImage.setImageFromURL(m!.imageURL, placeholder: UIImage(named: "eventDetailsPlaceholder"))
+            productImage?.setImageFromURL(m!.imageURL, placeholder: UIImage(named: "eventDetailsPlaceholder"))
+            configureAttendingInfo(m!.numOfParticipants ?? 0)
         case .News:
-            productImage.setImageFromURL(m!.imageURL, placeholder: UIImage(named: "placeholderEvent"))
+            productImage?.setImageFromURL(m!.imageURL, placeholder: UIImage(named: "placeholderEvent"))
+            configureAttendingInfo(m!.numOfParticipants ?? 0)
         case .Wallet:
-            productImage.setImageFromURL(m!.imageURL, placeholder: UIImage(named: "market_img_default"))
+            productImage?.setImageFromURL(m!.imageURL, placeholder: UIImage(named: "market_img_default"))
+            infoLabel?.text = m!.info
         case .Unknown:
             break
         default:
             break
         }
         
-        titleLabel.text = m!.title
-        let eventDetailsFormat = NSLocalizedString("%d People are attending", comment: "Event details: details format")
-        infoLabel.text = String(format: eventDetailsFormat, m!.numOfParticipants ?? 0)
-        dateLabel.text = m!.details
+        titleLabel?.text = m!.title
+        dateLabel?.text = m!.details
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
-        productImage.hnk_cancelSetImage()
+        productImage?.hnk_cancelSetImage()
     }
     
+    // MARK: - Private functions
+    private func configureAttendingInfo(numOfParticipants: Int) {
+        let attendingInfoFormat = NSLocalizedString("%d People are attending", comment: "Event details: details format")
+        infoLabel?.text = String(format: attendingInfoFormat, numOfParticipants)
+    }
 }
