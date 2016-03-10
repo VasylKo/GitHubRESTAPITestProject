7513144951b172d237a2dc45ab4211bf203cb191
Ruslan Kolchakov <rkolchakov@bekitzur.com>
2016-03-10 09:50:52 +0300

    Fixed conflict of merging

--- PositionIn/Views/FeedListCells/EventListCell.swift
+++ PositionIn/Views/FeedListCells/EventListCell.swift
@@ -25,41 +25,28 @@ final class EventListCell: TableViewCell {
         switch m!.item.type {
         case .Emergency:
             productImage?.setImageFromURL(m!.imageURL, placeholder: UIImage(named: "PromotionDetailsPlaceholder"))
-            configureAttendingInfo(m!.numOfParticipants ?? 0)
         case .GiveBlood:
             productImage?.setImageFromURL(m!.imageURL, placeholder: UIImage(named: "give_blood_img_default"))
-            configureAttendingInfo(m!.numOfParticipants ?? 0)
         case .Training:
             productImage?.setImageFromURL(m!.imageURL, placeholder: UIImage(named: "trainings_placeholder"))
-            configureAttendingInfo(m!.numOfParticipants ?? 0)
         case .Market:
             productImage?.setImageFromURL(m!.imageURL, placeholder: UIImage(named: "market_img_default"))
-            configureAttendingInfo(m!.numOfParticipants ?? 0)
         case .BomaHotels:
             productImage?.setImageFromURL(m!.imageURL, placeholder: UIImage(named: "bomaHotelPlaceholder"))
-            configureAttendingInfo(m!.numOfParticipants ?? 0)
         case .Project:
             productImage?.setImageFromURL(m!.imageURL, placeholder: UIImage(named: "hardware_img_default"))
-            configureAttendingInfo(m!.numOfParticipants ?? 0)
         case .Event:
             productImage?.setImageFromURL(m!.imageURL, placeholder: UIImage(named: "eventDetailsPlaceholder"))
-            configureAttendingInfo(m!.numOfParticipants ?? 0)
         case .News:
             productImage?.setImageFromURL(m!.imageURL, placeholder: UIImage(named: "placeholderEvent"))
-            configureAttendingInfo(m!.numOfParticipants ?? 0)
         case .Wallet:
             productImage?.setImageFromURL(m!.imageURL, placeholder: UIImage(named: "market_img_default"))
-            infoLabel?.text = m!.info
         case .Unknown:
             break
         default:
             break
         }
         
-<<<<<<< HEAD
-        titleLabel?.text = m!.title
-        dateLabel?.text = m!.details
-=======
         titleLabel.text = m!.title
         
         if m!.item.type == FeedItem.ItemType.Event {
@@ -78,17 +65,10 @@ final class EventListCell: TableViewCell {
             infoLabel.text = m!.info
             dateLabel.text = m!.details
         }
->>>>>>> develop
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
         productImage?.hnk_cancelSetImage()
-    }
-    
-    // MARK: - Private functions
-    private func configureAttendingInfo(numOfParticipants: Int) {
-        let attendingInfoFormat = NSLocalizedString("%d People are attending", comment: "Event details: details format")
-        infoLabel?.text = String(format: attendingInfoFormat, numOfParticipants)
     }
 }
