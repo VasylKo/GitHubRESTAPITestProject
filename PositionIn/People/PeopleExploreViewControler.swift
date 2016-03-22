3171d8fc4d0b0ccf9f6b40862294600216ce857c
Nikita Galimzyanov <ng@bekitzur.com>
2016-03-22 19:46:36 +0300

    controller separation

---
+++ PositionIn/People/PeopleExploreViewControler.swift
@@ -0,0 +1,21 @@
+//
+//  ExploreViewControler.swift
+//  PositionIn
+//
+//  Created by ng on 3/22/16.
+//  Copyright © 2016 Soluna Labs. All rights reserved.
+//
+
+import Foundation
+import PosInCore
+import BrightFutures
+import CleanroomLogger
+
+class PeopleExploreViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.redColor()
+    }
+    
+}
---
+++ PositionIn/People/PeopleFollowingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SubscriptionsViewController.swift
+//  PositionIn
+//
+//  Created by ng on 3/22/16.
+//  Copyright © 2016 Soluna Labs. All rights reserved.
+//
+
+import Foundation
+import PosInCore
+import BrightFutures
+import CleanroomLogger
+
+class PeopleFollowingViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.blueColor()
+    }
+    
+}
