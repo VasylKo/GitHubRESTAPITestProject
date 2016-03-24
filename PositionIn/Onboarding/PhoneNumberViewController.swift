84d358841cc4c1b71fc1e8ccea9114efd93e835e
Vasiliy Kotsiuba <vk@risingapp.com>
2016-03-24 16:12:08 +0300

    Changed alert text to: "Is your *mobile* phone number below correct?" and also text field placeholder text to: "Enter your *mobile* phone number"

--- PositionIn/Onboarding/PhoneNumberViewController.swift
+++ PositionIn/Onboarding/PhoneNumberViewController.swift
@@ -88,7 +88,7 @@ class PhoneNumberViewController: XLFormViewController {
         
         let phoneRow: XLFormRowDescriptor = XLFormRowDescriptor(tag: Tags.Phone.rawValue,
             rowType: XLFormRowDescriptorTypePhone)
-        phoneRow.cellConfigAtConfigure["textField.placeholder"] = "Enter your phone number"
+        phoneRow.cellConfigAtConfigure["textField.placeholder"] = "Enter your mobile phone number"
         phoneRow.required = true
         phoneRow.addValidator(XLFormRegexValidator(msg: NSLocalizedString("Please specify a valid phone number",
             comment: "Onboarding"), regex: "^\\+?\\d+$"))
@@ -139,7 +139,7 @@ class PhoneNumberViewController: XLFormViewController {
             
             let alertController = UIAlertController(title: NSLocalizedString("Number Confirmation",
                 comment: "Onboarding"),
-                message: "Is your phone number below correct?\n\(phoneNumber)", preferredStyle: .Alert)
+                message: "Is your mobile phone number below correct?\n\(phoneNumber)", preferredStyle: .Alert)
             
             let cancelAction = UIAlertAction(title: "Edit", style: .Cancel, handler: nil)
             alertController.addAction(cancelAction)
