55cf6d0b622683b56616b21b4bcf3ed4221fe433
Mikhail Polyevin <mpol@bekitzur.com>
2016-03-31 18:51:03 +0300

    POS-2224 Add PDF support in the app

---
+++ PositionIn/WebViewController/WebViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WebViewController.swift
+//  PositionIn
+//
+//  Created by Mikhail Polyevin on 31/03/16.
+//  Copyright © 2016 Soluna Labs. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+
+    @IBOutlet private weak var spinner: UIActivityIndicatorView!
+    @IBOutlet private weak var webView: UIWebView!
+    var contentURL: NSURL?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let url = contentURL {
+            let request = NSURLRequest(URL: url)
+                webView.loadRequest(request)
+            self.spinner.startAnimating()
+        }
+    }
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        self.spinner.stopAnimating()
+    }
+}
