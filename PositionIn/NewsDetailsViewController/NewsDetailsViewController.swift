a6fa636dfcc0cb143e4b952d00fd620a87b1a92b
Vasiliy Kotsiuba <vk@risingapp.com>
2016-04-06 16:27:33 +0300

    POS-2295: GA Feed events tracking added

--- PositionIn/NewsDetailsViewController/NewsDetailsViewController.swift
+++ PositionIn/NewsDetailsViewController/NewsDetailsViewController.swift
@@ -114,6 +114,7 @@ extension NewsDetailsViewController {
         comment.text = text
         
         api().createPostComment(post.objectId, object: comment).onSuccess {[weak self, weak textField] comment in
+            trackEventToAnalytics(AnalyticCategories.feedNews, action: AnalyticActios.comment, label: self?.post?.name ?? NSLocalizedString("Can't get news title"))
             self?.reloadPost()
             textField?.text = nil
         }
@@ -128,11 +129,13 @@ extension NewsDetailsViewController: NewsActionConsumer {
         if let tempPost = post {
             if (tempPost.isLiked) {
                 api().unlikePost(tempPost.objectId).onSuccess{[weak self] in
+                    trackEventToAnalytics(AnalyticCategories.feedNews, action: AnalyticActios.like, label: self?.post?.name ?? NSLocalizedString("Can't get news title"))
                     self?.reloadPost()
                 }
             }
             else {
                 api().likePost(tempPost.objectId).onSuccess{[weak self] in
+                    trackEventToAnalytics(AnalyticCategories.feedNews, action: AnalyticActios.unlike, label: self?.post?.name ?? NSLocalizedString("Can't get news title"))
                     self?.reloadPost()
                 }
             }
