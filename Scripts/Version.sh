fbbadf32838b78e3192661dd1d8c4a1e941e4395
Ruslan Kolchakov <rkolchakov@gmail.com>
2016-04-11 11:32:02 +0300

    no message

--- Scripts/Version.sh
+++ Scripts/Version.sh
@@ -5,31 +5,31 @@ echo "Get version ..."
 MAJOR_VERSION="${MAJOR_VERSION}.${MINOR_VERSION}.${FIX_NUMBER}"
 
 if [ -z "${MAJOR_VERSION}" ]; then
-MAJOR_VERSION="1.1.1"
+    MAJOR_VERSION="1.1.1"
 fi
 
 if [ -z "${PROJECT_DIR}" ]; then
-	PROJECT_DIR=`pwd`
+    PROJECT_DIR=`pwd`
 fi
 
 if [ -z "${PREFIX}" ]; then
-	PREFIX=""
+    PREFIX=""
 fi
 
 BUILD_HASH=$BUILD_NUMBER
 
 if [ -z "$1" ]; then
-	if [ "${BUILD_NUMBER}" == "${BUILD_HASH}" ]; then
-		echo "${MAJOR_VERSION}.${BUILD_NUMBER}"
-	else
-		echo "${MAJOR_VERSION}.${BUILD_NUMBER}.${BUILD_HASH}"
-	fi
+    if [ "${BUILD_NUMBER}" == "${BUILD_HASH}" ]; then
+    echo "${MAJOR_VERSION}.${BUILD_NUMBER}"
+    else
+    echo "${MAJOR_VERSION}.${BUILD_NUMBER}.${BUILD_HASH}"
+fi
 else
-	echo "#define ${PREFIX}BUILD_NUMBER ${BUILD_NUMBER}" > $1
-	echo "#define ${PREFIX}BUILD_HASH ${BUILD_HASH}" >> $1
+    echo "#define ${PREFIX}BUILD_NUMBER ${BUILD_NUMBER}" > $1
+    echo "#define ${PREFIX}BUILD_HASH ${BUILD_HASH}" >> $1
     echo "#define ${PREFIX}MAJOR_VERSION ${MAJOR_VERSION}" >> $1
-    echo "#define ${PREFIX}MINOR_VERSION ${MAJOR_VERSION}-${BUILD_NUMBER}" >> $1
-    echo "#define ${PREFIX}VERSION() @\"${MAJOR_VERSION}-${BUILD_NUMBER}\"" >> $1
-    echo "Version: ${MAJOR_VERSION}-${BUILD_NUMBER}.${BUILD_HASH}"
-	find "${PROJECT_DIR}" -iname "*.plist" -maxdepth 1 -exec touch {} \;	
+    echo "#define ${PREFIX}MINOR_VERSION ${MAJOR_VERSION}.${BUILD_NUMBER}" >> $1
+    echo "#define ${PREFIX}VERSION() @\"${MAJOR_VERSION}.${BUILD_NUMBER}\"" >> $1
+    echo "Version: ${MAJOR_VERSION}.${BUILD_NUMBER}.${BUILD_HASH}"
+    find "${PROJECT_DIR}" -iname "*.plist" -maxdepth 1 -exec touch {} \;
 fi
