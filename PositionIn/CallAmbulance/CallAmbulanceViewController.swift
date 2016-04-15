0cb864535688da7dbb6bf76ba7113f63c15724c8
Vasiliy Kotsiuba <vk@risingapp.com>
2016-04-15 17:50:03 +0300

    POS-2565: Added membership footer on call ambulance screen

--- PositionIn/CallAmbulance/CallAmbulanceViewController.swift
+++ PositionIn/CallAmbulance/CallAmbulanceViewController.swift
@@ -21,6 +21,8 @@ class CallAmbulanceViewController: BaseAddItemViewController {
         case Photo = "Photo"
     }
     
+    private var footerButtom: EplusSIgnUpNowButton?
+    
     private enum IncidentType: Int {
         case Other = 0, Fainted, Collapsed, NonResponsive, BreathingFast, Sweating, Bleeding, NotBreathing, NotTalking, Unconscious, Seizure, Choking, ChestPain
         
@@ -40,6 +42,7 @@ class CallAmbulanceViewController: BaseAddItemViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.tintColor = UIScheme.mainThemeColor
+        addFooterButton()
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -192,5 +195,28 @@ class CallAmbulanceViewController: BaseAddItemViewController {
 
     @IBAction func cancelButtonTouched(sender: AnyObject) {
         self.navigationController?.popToRootViewControllerAnimated(true)
+    }
+    
+    //MARK: - Footer button
+    private func addFooterButton() {
+        let buttonHeight = CGFloat(60)
+        let button = EplusSIgnUpNowButton(eplusButtonType: .SignUP)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: Selector("footerButtonTouched:"), forControlEvents: UIControlEvents.TouchUpInside)
+        view.insertSubview(button, aboveSubview: tableView)
+        
+        //Add buttons constaraints
+        let bottomConstraint = NSLayoutConstraint(item: view, attribute: .Bottom, relatedBy: .Equal, toItem: button, attribute: .Bottom, multiplier: 1.0, constant: 0.0)
+        let trailingConstraint = NSLayoutConstraint(item: view, attribute: .Trailing, relatedBy: .Equal, toItem: button, attribute: .Trailing, multiplier: 1.0, constant: 0.0)
+        let leadingConstraint = NSLayoutConstraint(item: view, attribute: .Leading, relatedBy: .Equal, toItem: button, attribute: .Leading, multiplier: 1.0, constant: 0.0)
+        let heightConstraint = NSLayoutConstraint(item: button, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: buttonHeight)
+        button.addConstraint(heightConstraint)
+        view.addConstraints([bottomConstraint, trailingConstraint, leadingConstraint])
+        
+        footerButtom = button
+    }
+    
+    func footerButtonTouched(sender: UIButton) {
+        
     }
 }
---
+++ PositionIn/Classes/Application Layer/Stories/EplusMembership/Interface/EplusSIgnUpNowButton/EplusSIgnUpNowButton.swift
@@ -0,0 +1,72 @@
+//
+//  EplusSIgnUpNowButton.swift
+//  PositionIn
+//
+//  Created by Vasiliy Kotsiuba on 15/04/16.
+//  Copyright © 2016 Soluna Labs. All rights reserved.
+//
+
+import UIKit
+
+class EplusSIgnUpNowButton: UIButton {
+
+
+    @IBOutlet weak var signUpNowViewContainer: UIView!
+    @IBOutlet weak var viewPlanViewContainer: UIView!
+    enum EplusButtonType {
+        case SignUP, AlreadyMember
+    }
+    
+    var type: EplusButtonType = .SignUP {
+        didSet {
+            setupUI()
+        }
+    }
+
+
+    // MARK: init methods
+    convenience init(eplusButtonType: EplusButtonType) {
+        self.init()
+        type = eplusButtonType
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonSetup()
+        setupUI()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        commonSetup()
+    }
+    
+    // MARK: setup view
+    private func commonSetup() {
+        let nibView = loadViewFromNib()
+        nibView.frame = bounds
+        addSubViewOnEntireSize(nibView)
+        nibView.userInteractionEnabled = false
+    }
+    
+    
+    private func loadViewFromNib() -> UIView {
+        let viewBundle = NSBundle(forClass: self.dynamicType)
+        //  An exception will be thrown if the xib file with this class name not found,
+        let view = viewBundle.loadNibNamed(String(self.dynamicType), owner: self, options: nil)[0]
+        return view as! UIView
+    }
+    
+    private func setupUI() {
+        switch type {
+        case .AlreadyMember:
+            viewPlanViewContainer.hidden = false
+            signUpNowViewContainer.hidden = true
+        case .SignUP:
+            viewPlanViewContainer.hidden = true
+            signUpNowViewContainer.hidden = false
+        }
+    }
+
+}
