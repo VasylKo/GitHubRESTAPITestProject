1188b9b886866fe1fd84413e34d08863a70aa8f6
Vasiliy Kotsiuba <clic@ukr.net>
2016-04-23 08:54:27 +0300

    POS-2396: added project intro VC + assets

---
+++ PositionIn/Projects/ProjectsIntroViewController/ProjectsIntroViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ProjectsIntroViewController.swift
+//  PositionIn
+//
+//  Created by Vasyl Kotsiuba on 4/23/16.
+//  Copyright © 2016 Soluna Labs. All rights reserved.
+//
+
+import UIKit
+
+class ProjectsIntroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
