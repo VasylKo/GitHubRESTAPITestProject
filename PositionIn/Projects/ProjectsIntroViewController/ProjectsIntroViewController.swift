c7b6a7353505d040640be62d7996e71e521d16a0
Vasiliy Kotsiuba <clic@ukr.net>
2016-04-23 11:04:53 +0300

    POS-2396: added content to Project Into VC

--- PositionIn/Projects/ProjectsIntroViewController/ProjectsIntroViewController.swift
+++ PositionIn/Projects/ProjectsIntroViewController/ProjectsIntroViewController.swift
@@ -8,14 +8,20 @@
 
 import UIKit
 
+let projectsIntroShowedKey = "projectsIntroShowedKey"
+
 class ProjectsIntroViewController: UIViewController {
     
     weak var browseGridDelegate: BrowseGridViewControllerDelegate?
+    
+    @IBOutlet weak var titleLabel: UILabel?
+    @IBOutlet weak var bodyTextLabel: UILabel?
     
     // MARK: - View life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupInterface()
+        setText()
         
     }
 
@@ -27,8 +33,17 @@ class ProjectsIntroViewController: UIViewController {
         
     }
     
+    private func setText() {
+        let title = NSLocalizedString("Маке а difference, be a difference", comment: "Project intro title")
+        let text = NSLocalizedString(
+            "18 million people in Kenya are living in desolation due to poverty, drought, floods, poor health, malnutrition, lack of clean water & sanitation, education and much more.\n\nThrough various programmes, KRCS has increased community resilience through the strengthening, diversification and protection of livelihoods and assets.\n\nThe interventions have an integrated approach through programming for improved livelihoods with a holistic view of access to water, irrigation, agriculture and by extension, health. Stay up to date with our latest KRCS activities", comment: "Project intro text")
+        
+        titleLabel?.text = title
+        bodyTextLabel?.text = text
+    }
+    
     // MARK: - Actions
-    @objc func nextButtonTouched(sender: AnyObject) {
+    @IBAction func nextButtonTouched(sender: AnyObject) {
         showProjectsList()
     }
     
