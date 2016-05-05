99759010adcf6fe4415231ce9665c0d1c0039379
Vasiliy Kotsiuba <clic@ukr.net>
2016-05-05 16:18:01 +0300

    POS-2756: added project structure for Give Blood story

---
+++ PositionIn/Classes/Application Layer/Stories/GiveBlood/Router/GiveBloodRouter.swift
@@ -0,0 +1,33 @@
+//
+//  MembershipRouter.swift
+//  PositionIn
+//
+//  Created by ng on 1/26/16.
+//  Copyright © 2016 Soluna Labs. All rights reserved.
+//
+
+import Foundation
+
+protocol GiveBloodRouter : BaseRouter {
+    /*
+    func showInitialViewController(from sourceViewController : UIViewController, hasActivePlan: Bool?)
+    
+    func showMembershipPlanDetailsViewController(from sourceViewController : UIViewController, with plan : MembershipPlan, onlyPlanInfo : Bool)
+    
+    func showMembershipMemberProfile(from sourceViewController : UIViewController, phoneNumber : String, validationCode : String)
+
+    func showMembershipMemberCardViewController(from sourceViewController : UIViewController, showBackButton: Bool)
+
+    func showMembershipConfirmDetailsViewController(from sourceViewController : UIViewController, with plan : MembershipPlan)
+    
+    func showPaymentViewController(from sourceViewController : UIViewController, with plan : MembershipPlan)
+    
+    func showPlansViewController(from sourceViewController : UIViewController, with plan : MembershipPlan)
+    
+    func showMemberDetailsViewController(from sourceViewController : UIViewController)
+    
+    func showMembershipPaymentTransactionViewController(from sourceViewController : UIViewController, withPaymentSystem : PaymentSystem)
+    
+    func dismissMembership(from sourceViewController : UIViewController)
+ */
+}
