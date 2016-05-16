23c8ddb0aa92df6c4bcd07e325109c768e918560
Ruslan Kolchakov <rkolchakov@bekitzur.com>
2016-05-16 15:13:20 +0300

    Trying to fix build launch

--- Applications/RCKenya/AppDelegate.swift
+++ Applications/RCKenya/AppDelegate.swift
@@ -79,9 +79,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        Fabric.with([Crashlytics.self])
-        return true
-        
         setupMaps()
         api.defaultErrorHandler = UIErrorHandler()
         if SearchFilter.isCustomLocationSet == false {
