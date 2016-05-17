038874b9e8fa3e2da9ceba5b89d56329fda87037
Vasiliy Kotsiuba <vk@risingapp.com>
2016-05-17 14:39:44 +0300

    POS-2746: Added new attachments show logic to Projects Screen

--- PositionIn/ProductDetails/ProductDetailsViewController.swift
+++ PositionIn/ProductDetails/ProductDetailsViewController.swift
@@ -19,6 +19,7 @@ final class ProductDetailsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor.groupTableViewBackgroundColor()
         title = NSLocalizedString("Project", comment: "Project details: title")
         dataSource.items = productAcionItems()
         dataSource.configureTable(actionTableView)
@@ -51,6 +52,16 @@ final class ProductDetailsViewController: UIViewController {
                     self?.didReceiveProductDetails(product)
                     strongSelf.dataSource.items = strongSelf.productAcionItems()
                     strongSelf.dataSource.configureTable(strongSelf.actionTableView)
+                    
+                    //If there is only 1 attachment, thant show it on current screen
+                    if let numberOfAttachments = strongSelf.product?.numberOfAttachments where numberOfAttachments == 1 {
+                        strongSelf.actionTableViewHeightConstraint.constant = strongSelf.actionTableView.contentSize.height
+                        strongSelf.addAttachmentSection()
+                    } else {
+                        //adjust scroll view content size based on table view height
+                        let scrollViewBottomSpaceHeight: CGFloat = 20
+                        strongSelf.actionTableViewHeightConstraint.constant = strongSelf.actionTableView.contentSize.height + scrollViewBottomSpaceHeight
+                    }
                 }
             }
         default:
@@ -103,6 +114,12 @@ final class ProductDetailsViewController: UIViewController {
         return dataSource
         }()
     
+    private func addAttachmentSection() {
+        attechmentSectionHeightConstraint.constant = MoreInformationViewController.singleAttacmentViewHeight
+        let moreInformationViewController = MoreInformationViewController(links: self.product?.links, attachments: self.product?.attachments, bounces: false)
+        let attachmentsView = moreInformationViewController.view
+        attechmentSectionView.addSubview(attachmentsView)
+    }
     
     private func productAcionItems() -> [[ProductActionItem]] {
         let zeroSection = [ // 0 section
@@ -121,7 +138,7 @@ final class ProductDetailsViewController: UIViewController {
         if self.product?.location != nil {
             firstSection.append(ProductActionItem(title: NSLocalizedString("Navigate", comment: "Product action: Navigate"), image: "productNavigate", action: .Navigate))
         }
-        if self.product?.links?.isEmpty == false || self.product?.attachments?.isEmpty == false {
+        if let numberOfAttachments = product?.numberOfAttachments where numberOfAttachments > 1 {
             firstSection.append(ProductActionItem(title: NSLocalizedString("Attachments"), image: "productTerms&Info", action: .MoreInformation))
         }
         
@@ -129,9 +146,12 @@ final class ProductDetailsViewController: UIViewController {
     }
     
     @IBOutlet private weak var actionTableView: UITableView!
+    @IBOutlet weak var actionTableViewHeightConstraint: NSLayoutConstraint!
     @IBOutlet private weak var productImageView: UIImageView!
     @IBOutlet private weak var headerLabel: UILabel!
     @IBOutlet private weak var infoLabel: UILabel!
+    @IBOutlet weak var attechmentSectionView: UIView!
+    @IBOutlet weak var attechmentSectionHeightConstraint: NSLayoutConstraint!
     
     @IBOutlet weak var pinDistanceImageView: UIImageView!
     @IBOutlet private weak var nameLabel: UILabel!
