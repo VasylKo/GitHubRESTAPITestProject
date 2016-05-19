79e08109bc09f4d09e80eb48214d9f7f989a1934
Ruslan Kolchakov <rkolchakov@bekitzur.com>
2016-05-19 18:49:29 +0300

    Removed not used dependencies from za

--- Applications/RCSouthAfrica/AppDelegate.swift
+++ Applications/RCSouthAfrica/AppDelegate.swift
@@ -15,10 +15,6 @@ import Messaging
 import GoogleMaps
 import XLForm
 import Braintree
-import Fabric
-import Crashlytics
-import LNNotificationsUI
-
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -142,9 +138,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         UIApplication.sharedApplication().registerUserNotificationSettings(settings)
         UIApplication.sharedApplication().registerForRemoteNotifications()
         
-        #if DEBUG
-            Fabric.with([Crashlytics.self])
-        #endif
         NewRelicController.sharedInstance.start()
         
         let notificationSettings = LNNotificationAppSettings()
